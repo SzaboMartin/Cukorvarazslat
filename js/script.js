084a138bcb12e5f5f7b631b51c830f3fddbd3d47
Foldi Akos <foldi.akosadam-2019@keri.mako.hu>
2024-04-29 15:53:57 +0200

    ad

--- js/script.js
+++ js/script.js
@@ -1,21 +1,4 @@
 
-//Megosztás ikon
-const shareBtn = document.getElementById('shareBtn');
-
-shareBtn.addEventListener('click', event => {
-
-  if (navigator.share) {
-    navigator.share({
-      text: 'Please read this great article: ',
-      url: './Cukorvarazslat/Cukraszda.html'
-    }).then(() => {
-      console.log('Thanks for sharing!');
-    })
-      .catch((err) => console.error(err));
-  } else {
-    alert("The current browser does not support the share function. Please, manually share the link")
-  }
-});
 
 //Kosár ikon
 let openShopping = document.querySelector('.shopping');
