87e00eb7f41cca169133611d069b94debb0eb63e
Foldi Akos <foldi.akosadam-2019@keri.mako.hu>
2024-11-22 14:04:17 +0100

    ak

--- db/cukorvarazslat.sql
+++ db/cukorvarazslat.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2024. Máj 05. 21:11
+-- Létrehozás ideje: 2024. Jún 12. 20:41
 -- Kiszolgáló verziója: 10.4.32-MariaDB
 -- PHP verzió: 8.2.12
 
@@ -40,65 +40,9 @@ CREATE TABLE `offers` (
 --
 
 INSERT INTO `offers` (`id`, `name`, `email`, `message`, `date`) VALUES
-(5, 'Ódry Attila', 'odry.attila@keri.mako.hu', 'dsdsdsdsds', '2024-04-29');
-
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `orders`
---
-
-CREATE TABLE `orders` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `card_number` varchar(20) NOT NULL,
-  `expiration` varchar(7) NOT NULL,
-  `cvv` varchar(3) NOT NULL,
-  `date` datetime NOT NULL,
-  `total` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- A tábla adatainak kiíratása `orders`
---
-
-INSERT INTO `orders` (`id`, `user_id`, `card_number`, `expiration`, `cvv`, `date`, `total`) VALUES
-(1, 15, '1111111111111111', '2024/05', '123', '2024-05-04 18:24:15', 49984),
-(2, 15, '1111111111111111', '2025/01', '123', '2024-05-04 18:27:18', 20995),
-(3, 15, '1111111111111111', '2024/05', '123', '2024-05-04 20:34:45', 5998),
-(4, 15, '1111111111111111', '2024/05', '123', '2024-05-05 09:57:43', 10797),
-(5, 17, '0132132313101212', '2026/03', '510', '2024-05-05 19:49:57', 5998),
-(6, 17, '0132132313101212', '2026/03', '510', '2024-05-05 20:50:33', 5997);
-
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `orders_item`
---
-
-CREATE TABLE `orders_item` (
-  `id` int(11) NOT NULL,
-  `order_id` int(11) NOT NULL,
-  `product_id` int(11) NOT NULL,
-  `quantity` tinyint(4) NOT NULL,
-  `price` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- A tábla adatainak kiíratása `orders_item`
---
-
-INSERT INTO `orders_item` (`id`, `order_id`, `product_id`, `quantity`, `price`) VALUES
-(1, 1, 2, 12, 2999),
-(2, 1, 7, 3, 3999),
-(3, 1, 24, 1, 1999),
-(4, 2, 2, 2, 2999),
-(5, 2, 8, 3, 4999),
-(6, 3, 2, 2, 2999),
-(7, 4, 3, 2, 3999),
-(8, 4, 29, 1, 2799),
-(9, 5, 2, 2, 2999),
-(10, 6, 10, 3, 1999);
+(5, 'Ódry Attila', 'odry.attila@keri.mako.hu', 'dsdsdsdsds', '2024-04-29'),
+(6, 'Ákos', 'akoska610@gmail.com', 'szia', '2024-06-12'),
+(7, 'akoss', 'akoska610@gmail.com', 'heloka', '2024-06-12');
 
 -- --------------------------------------------------------
 
@@ -173,88 +117,6 @@ INSERT INTO `products_type` (`id`, `type`, `name`, `order_by`) VALUES
 (3, 'figurak', 'Figurák', 1),
 (4, 'naszparok', 'Nászpárok', 3);
 
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `shopping_cart`
---
-
-CREATE TABLE `shopping_cart` (
-  `user_id` int(11) NOT NULL,
-  `product_id` int(11) NOT NULL,
-  `quantity` tinyint(3) UNSIGNED NOT NULL,
-  `price` int(11) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `type`
---
-
-CREATE TABLE `type` (
-  `id` char(1) NOT NULL,
-  `type` varchar(10) NOT NULL,
-  `name` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- A tábla adatainak kiíratása `type`
---
-
-INSERT INTO `type` (`id`, `type`, `name`) VALUES
-('F', 'GENDER', 'nő'),
-('M', 'GENDER', 'férfi'),
-('A', 'USER', 'adminisztátor'),
-('G', 'USER', 'vendég'),
-('L', 'USER', 'vezető'),
-('N', 'USER', 'nincs hitelesítve'),
-('U', 'USER', 'felhasználó'),
-('W', 'USER', 'dolgozó');
-
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `users`
---
-
-CREATE TABLE `users` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `type` char(1) NOT NULL DEFAULT 'G',
-  `name` varchar(100) NOT NULL,
-  `born` date DEFAULT NULL,
-  `gender` char(1) NOT NULL,
-  `country_code` varchar(3) DEFAULT NULL,
-  `phone` varchar(20) DEFAULT NULL,
-  `address` text DEFAULT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(20) NOT NULL,
-  `valid` tinyint(1) UNSIGNED NOT NULL DEFAULT 1
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- A tábla adatainak kiíratása `users`
---
-
-INSERT INTO `users` (`id`, `type`, `name`, `born`, `gender`, `country_code`, `phone`, `address`, `email`, `password`, `valid`) VALUES
-(1, 'U', 'Bálint Bence Dániel', NULL, 'M', NULL, NULL, NULL, 'balint.bencedaniel-2019@keri.mako.hu', '1234Aa', 1),
-(2, 'U', 'Bálint Gergő', NULL, 'M', NULL, NULL, NULL, 'balint.gergo-2019@keri.mako.hu', '1234Aa', 1),
-(3, 'U', 'Földi Ákos Ádám', NULL, 'M', NULL, NULL, NULL, 'foldi.akosadam-2019@keri.mako.hu', '1234Aa', 1),
-(4, 'U', 'Kányási Dávid Gergő', NULL, 'M', NULL, NULL, NULL, 'kanyasi.davidgergo-2019@keri.mako.hu', '1234Aa', 1),
-(5, 'U', 'Kiss Benjámin Zoltán', NULL, 'M', NULL, NULL, NULL, 'kiss.benyaminzoltan-2019@keri.mako.hu', '1234Aa', 1),
-(6, 'U', 'Surányi Csenge', NULL, 'F', NULL, NULL, NULL, 'suranyi.csenge-2019@keri.mako.hu', '1234Aa', 1),
-(7, 'U', 'Szabó Martin Tamás', NULL, 'M', NULL, NULL, NULL, 'szabo.martintamas-2019@keri.mako.hu', '1234Aa', 1),
-(8, 'U', 'Tóth Barnabás Gyula', NULL, 'M', NULL, NULL, NULL, 'toth.barnabasgyula-2019@keri.mako.hu', '1234Aa', 1),
-(9, 'U', 'Török Ferenc', NULL, 'M', NULL, NULL, NULL, 'torok.ferenc-2019@keri.mako.hu', '1234Aa', 1),
-(10, 'U', 'Valach Martin', NULL, 'M', NULL, NULL, NULL, 'valach.martin-2019@keri.mako.hu', '1234Aa', 1),
-(11, 'U', 'Vörös Alexandra', NULL, 'F', NULL, NULL, NULL, 'voros.alexandra-2019@keri.mako.hu', '1234Aa', 1),
-(12, 'A', 'Gera Imre', NULL, 'M', NULL, NULL, NULL, 'gera.imre.tch@gmail.com', '1234Aa', 1),
-(13, 'A', 'Kiss Andrea', '1974-12-01', 'F', '36', '302360141', '6900 Makó, Posta utca 4-6.', 'kissamail@gmail.com', '1234Aa', 1),
-(14, 'A', 'Molnár Zsolt', NULL, 'M', NULL, NULL, NULL, 'molnarzsoltnavay@gmail.com', '1234Aa', 1),
-(15, 'A', 'Ódry Attila', '1964-03-08', 'M', '36', '304816888', '6725 Szeged, Futrinka utca 66.', 'odry.attila@keri.mako.hu', '1234Aa', 1),
-(16, 'A', 'Vidéki Gyula Milán', NULL, 'M', NULL, NULL, NULL, 'videki.gyula.milan@gmail.com', '1234Aa', 1),
-(17, 'U', 'Földi Ákos', '2004-03-17', 'M', '36', '301168611', 'Szegedi utca 9-13/c', 'akoska610@gmail.com', 'Majompok1', 1);
-
 --
 -- Indexek a kiírt táblákhoz
 --
@@ -263,18 +125,6 @@ INSERT INTO `users` (`id`, `type`, `name`, `born`, `gender`, `country_code`, `ph
 -- A tábla indexei `offers`
 --
 ALTER TABLE `offers`
-  ADD PRIMARY KEY (`id`);
-
---
--- A tábla indexei `orders`
---
-ALTER TABLE `orders`
-  ADD PRIMARY KEY (`id`);
-
---
--- A tábla indexei `orders_item`
---
-ALTER TABLE `orders_item`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -290,25 +140,6 @@ ALTER TABLE `products_type`
   ADD PRIMARY KEY (`id`);
 
 --
--- A tábla indexei `shopping_cart`
---
-ALTER TABLE `shopping_cart`
-  ADD PRIMARY KEY (`user_id`,`product_id`,`price`) USING BTREE;
-
---
--- A tábla indexei `type`
---
-ALTER TABLE `type`
-  ADD PRIMARY KEY (`type`,`id`) USING BTREE;
-
---
--- A tábla indexei `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
-
---
 -- A kiírt táblák AUTO_INCREMENT értéke
 --
 
@@ -316,19 +147,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT a táblához `offers`
 --
 ALTER TABLE `offers`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT a táblához `orders`
---
-ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT a táblához `orders_item`
---
-ALTER TABLE `orders_item`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT a táblához `products`
@@ -341,12 +160,6 @@ ALTER TABLE `products`
 --
 ALTER TABLE `products_type`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT a táblához `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
